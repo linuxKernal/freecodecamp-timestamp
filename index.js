fd9e2f547e26af7c478bce18c1351cf236f3bb5f
praveenpothiraj <praveenpothiraj123@gmail.com>
2023-04-01 22:03:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+// index.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+app.get("/api/:date", function (req, res) {
+  let date = Date.parse(req.params.date);
+  const unix = Number(req.params.date);
+  if(!date && !unix) res.send({ error : "Invalid Date" })
+  if(unix){
+    res.json({"unix":unix,"utc":new Date(unix).toUTCString()});
+  }else 
+  res.json({"unix":date,"utc":new Date(req.params.date).toUTCString()});
+}).get("/api",(req,res)=>{
+    res.send({"unix":Date.now(),"utc":new Date().toUTCString()})
+})
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
